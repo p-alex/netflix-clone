b27a410afe2ea021c5aa4f6c0425a751ec16f934
p-alex <alexey9021@gmail.com>
2021-07-03 23:29:52 +0300

    Implemented register functionality
    -stored user in database
    -hashed the password
    -validation

---
+++ pages/api/auth.js
@@ -0,0 +1,63 @@
+import { MongoClient } from "mongodb";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+const authHandler = async (req, res) => {
+  const client = await MongoClient.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db();
+  const collection = db.collection("users");
+  if (req.method === "POST") {
+    try {
+      if (req.body.isRegister) {
+        const { username, email, password, confirmPassword } = req.body;
+        console.log(req.body);
+        const user = await collection.findOne({ email });
+        console.log(user);
+        if (user) return res.json({ message: "That user already exists" });
+
+        if (password !== confirmPassword)
+          return res.json({ message: "Passwords must match" });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        await collection.insertOne({
+          username,
+          email,
+          password: hashedPassword,
+        });
+
+        res.json({ message: "Registered successfuly!" });
+      } else {
+        const { email, password } = req.body;
+
+        const user = await collection.find({ email });
+
+        if (!user)
+          return res.json({
+            message: "The user with that email doesn't exist",
+          });
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) return res.json({ message: "Wrong password" });
+
+        if (user && isValidPassword) {
+          const token = await jwt.sign(
+            { username: user.username, email: user.email, id: user._id },
+            process.env.SECRET,
+            { expiresIn: "15m" }
+          );
+
+          res.json({ message: "Logged in!", token });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res.json({ message: "Something went wrong..." });
+    }
+  }
+};
+
+export default authHandler;
--- pages/register.js
+++ pages/register.js
@@ -1,22 +1,77 @@
+import { useState } from "react";
 import styles from "../styles/Register.module.css";
 import Link from "next/link";
 export default function register() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isRegister: true,
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      inputs.username &&
+      inputs.email &&
+      inputs.password &&
+      inputs.confirmPassword
+    ) {
+      try {
+        const result = await fetch("http://localhost:3000/api/auth", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(inputs),
+        });
+
+        const resultJSON = await result.json();
+
+        console.log(resultJSON);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div
       className={styles.register_wrapper}
       style={{ backgroundImage: "url(/images/bg/auth-bg.jpg)" }}
     >
       <div className={styles.black_overlay}></div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.logo}>
           <img src="/images/logo/netflix-logo.png" alt="" />
         </div>
         <div className={styles.input_group}>
-          <input type="text" name="username" id="username" placeholder=" " />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+            required
+          />
           <label for="username">Username</label>
         </div>
         <div className={styles.input_group}>
-          <input type="email" name="email" id="email" placeholder=" " />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+            required
+          />
           <label for="email">E-mail</label>
         </div>
         <div className={styles.input_group}>
@@ -25,6 +80,8 @@ export default function register() {
             name="password"
             id="password"
             placeholder=" "
+            onChange={(e) => handleChange(e)}
+            required
           />
           <label for="password">Password</label>
         </div>
@@ -34,6 +91,8 @@ export default function register() {
             name="confirmPassword"
             id="confirmPassword"
             placeholder=" "
+            onChange={(e) => handleChange(e)}
+            required
           />
           <label for="confirmPassword">Confirm password</label>
         </div>
