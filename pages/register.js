3d21a590c65cba60fefd4cbc52c5474cc43f2c3e
p-alex <alexey9021@gmail.com>
2021-07-06 22:19:33 +0300

    Replaced form, input groups and submit button with components in register page

--- pages/register.js
+++ pages/register.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { useRouter } from "next/router";
 import styles from "../styles/Register.module.css";
 import Link from "next/link";
+import Form from "../components/Form";
+import InputGroup from "../components/InputGroup";
+import SubmitButton from "../components/SubmitButton";
 export default function register() {
-  const router = useRouter();
-
   const [inputs, setInputs] = useState({
     username: "",
     email: "",
@@ -12,6 +12,28 @@ export default function register() {
     confirmPassword: "",
     isRegister: true,
   });
+  const inputList = [
+    {
+      label: "Username",
+      setNameId: "username",
+      type: "text",
+      placeholder: " ",
+    },
+    { label: "E-mail", setNameId: "email", type: "email", placeholder: " " },
+    {
+      label: "Password",
+      setNameId: "password",
+      type: "password",
+      placeholder: " ",
+    },
+    {
+      label: "Confirm Password",
+      setNameId: "confirmPassword",
+      type: "password",
+      placeholder: " ",
+    },
+  ];
+  const [feedback, setFeedback] = useState("");
 
   const handleChange = (e) => {
     setInputs((prevState) => ({
@@ -34,13 +56,16 @@ export default function register() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(inputs),
+          body: JSON.stringify({ ...inputs, date: Date.now() }),
         });
 
         const resultJSON = await result.json();
 
-        if (resultJSON.message === "Registered successfuly!")
-          router.push("/login");
+        resultJSON.message === "Registered successfuly!"
+          ? setFeedback(
+              "We sent you an email to verify your account! Check your email"
+            )
+          : setFeedback(resultJSON.message);
 
         console.log(resultJSON);
       } catch (error) {
@@ -54,59 +79,29 @@ export default function register() {
       style={{ backgroundImage: "url(/images/bg/auth-bg.jpg)" }}
     >
       <div className={styles.black_overlay}></div>
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <Form submitFunc={handleSubmit}>
         <div className={styles.logo}>
           <img src="/images/logo/netflix-logo.png" alt="" />
         </div>
-        <div className={styles.input_group}>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder=" "
-            onChange={(e) => handleChange(e)}
-            required
-          />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div className={styles.input_group}>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder=" "
-            onChange={(e) => handleChange(e)}
-            required
-          />
-          <label htmlFor="email">E-mail</label>
-        </div>
-        <div className={styles.input_group}>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder=" "
-            onChange={(e) => handleChange(e)}
-            required
-          />
-          <label htmlFor="password">Password</label>
-        </div>
-        <div className={styles.input_group}>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            placeholder=" "
-            onChange={(e) => handleChange(e)}
-            required
-          />
-          <label htmlFor="confirmPassword">Confirm password</label>
-        </div>
-        <button type="submit">Register</button>
+        {feedback && <p>{feedback}</p>}
+        {inputList.map((input) => {
+          return (
+            <InputGroup
+              key={input.label}
+              setId={input.setNameId}
+              setType={input.type}
+              setPlaceholder={input.placeholder}
+              setName={input.setNameId}
+              setLabel={input.label}
+              handleChangeFunc={handleChange}
+            />
+          );
+        })}
+        <SubmitButton>Register</SubmitButton>
         <p>
           Already have an account? <Link href="/login">Login now</Link>
         </p>
-      </form>
+      </Form>
     </div>
   );
 }
