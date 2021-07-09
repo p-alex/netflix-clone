11246912ce550ea23abe0285edcfb3bd4677f1e6
p-alex <alexey9021@gmail.com>
2021-07-09 18:45:07 +0300

    Fetching data from the movies end point on the server

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,45 @@
-import Head from "next/head";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-export default function Home() {
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    setUser(JSON.parse(localStorage.getItem("user")));
-  }, []);
+export default function Home({ result }) {
   return (
     <div>
       <h1>Netflix Clone</h1>
-      <p>Welcome, {user.username}</p>
+      <p>Result: {JSON.stringify(result)}</p>
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  let { token } = context.req.cookies;
+  if (token) {
+    let result = await fetch("http://localhost:3000/api/movies", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    let resultJSON = await result.json();
+    if (resultJSON.message === "Not allowed") {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: true,
+        },
+      };
+    }
+    return {
+      props: {
+        result: resultJSON,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+};
