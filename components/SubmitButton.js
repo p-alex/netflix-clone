3e1878669798ee30216ffab1575e933dfa2208e4
p-alex <alexey9021@gmail.com>
2021-07-11 19:59:23 +0300

    Update SubmitButton.js

--- components/SubmitButton.js
+++ components/SubmitButton.js
@@ -1,7 +1,7 @@
 import styles from "../styles/SubmitButton.module.css";
 export default function SubmitButton({ children }) {
   return (
-    <button className={styles.submitButton} type="submit">
+    <button className={styles.submitButton} type="submit" aria-label={children}>
       {children}
     </button>
   );
