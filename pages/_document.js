9cb54111f0364994b35131613b791ce1e873f9cf
p-alex <alexey9021@gmail.com>
2021-07-20 12:36:51 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
