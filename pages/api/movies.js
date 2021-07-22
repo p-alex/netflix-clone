5cfc5fc82c1f1b89ecbfcab2d1cf3a4b69501cec
p-alex <alexey9021@gmail.com>
2021-07-22 16:14:54 +0300

    Update movies.js

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,24 +1,24 @@
 import { MongoClient, ObjectId } from "mongodb";
 import withProtect from "../../middleware/withProtect";
 async function moviesHandler(req, res) {
+  const client = await MongoClient.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   if (req.method === "GET") {
-    const client = await MongoClient.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
     const db = client.db();
     const collection = db.collection("users");
     const moviesCollection = db.collection("movies");
 
     try {
       const user = await collection.findOne({ _id: ObjectId(req.userId) });
-      const movies = await moviesCollection.find({});
+      const movies = await moviesCollection.find({}).toArray();
       if (!user.username) {
         return res.json({ message: `User doesn't exist` });
       }
       return res.json({
         message: "movies..",
-        movies: await movies.toArray(),
+        movies,
       });
     } catch {
       return res.json({ message: "Something went wrong..." });
