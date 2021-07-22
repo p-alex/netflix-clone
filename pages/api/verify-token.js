fdb5814a362101f7f387920f73a5f658b9007424
p-alex <alexey9021@gmail.com>
2021-07-22 15:55:16 +0300

    Update verify-token.js

--- pages/api/verify-token.js
+++ pages/api/verify-token.js
@@ -5,12 +5,11 @@ export default async function verifyTokenHandler(req, res) {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  const collection = await client.db().collection("users");
+  const collection = client.db().collection("users");
   if (req.method === "POST") {
     try {
       const token = req.headers.authorization.split(" ")[1];
       if (token) {
-        console.log(token);
         let decoded = await jwt.verify(token, process.env.SECRET);
         if (decoded?.id) {
           const user = collection.findOne({ _id: ObjectId(decoded.id) });
