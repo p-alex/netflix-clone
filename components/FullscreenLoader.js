512aa9bbe85565deb8dc5929f7fa42c6ed4e49e0
p-alex <alexey9021@gmail.com>
2021-08-08 21:47:20 +0300

    Changed css class naming for FullscreenLoader

--- components/FullscreenLoader.js
+++ components/FullscreenLoader.js
@@ -1,8 +1,8 @@
 import styles from "../styles/FullscreenLoader.module.css";
 export default function FullscreenLoader() {
   return (
-    <div className={styles.fullscreenLoader}>
-      <div className={styles.loader}>
+    <div className={styles.loader}>
+      <div className={styles.loader__logo}>
         <img src={"/images/logo/netflix-logo.png"} alt="" />
       </div>
     </div>
