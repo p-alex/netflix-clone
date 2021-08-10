0378731bf10a2a58ad37719afd74f69284ebda99
p-alex <alexey9021@gmail.com>
2021-08-10 14:13:00 +0300

    Changed Modal css class naming and added 'more like this' section

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import ProjectContext from "../context/Project-context";
 import styles from "../styles/Modal.module.css";
 export default function Modal({ movie }) {
+  const [moreLikeThisArray, setMoreLikeThisArray] = useState([]);
   const {
     name,
     nameSlug,
@@ -16,112 +17,175 @@ export default function Modal({ movie }) {
     directors,
   } = movie;
   const context = useContext(ProjectContext);
-  const { handleResetSelectedMovie } = context;
+  const { handleResetSelectedMovie, allMovies } = context;
+  useEffect(() => {
+    setMoreLikeThisArray(
+      allMovies.filter((mov) => {
+        if (mov.name !== movie.name) {
+          if (
+            mov.genres.some((m) => movie.genres.includes(m)) &&
+            mov.thisMovieIs.some((t) => movie.thisMovieIs.includes(t))
+          ) {
+            return mov;
+          }
+        }
+      })
+    );
+    return () => {
+      console.log("Modal unmounted");
+    };
+  }, []);
   return (
     <>
       {movie.name && (
         <>
-          <div
-            className={styles.modal__backdrop}
-            onClick={handleResetSelectedMovie}
-          ></div>
-          <div className={styles.modal}>
-            <button
-              className={styles.modal__closeBtn}
+          <div className={styles.modal__container}>
+            <div
+              className={styles.modal__backdrop}
               onClick={handleResetSelectedMovie}
-            >
-              X
-            </button>
-            <div className={styles.modal__header}>
-              <div className={styles.modal__video}>
-                <img
-                  className={styles.modal__videoImage}
-                  src={`/movies/${nameSlug}/${nameSlug}-video-cover.jpg`}
-                  alt=""
-                />
-                <img
-                  className={styles.modal__movieLogo}
-                  src={`/movies/${nameSlug}/${nameSlug}-logo.png`}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className={styles.modal__info}>
-              <div className={styles.info__container}>
-                <div className={styles.info__stats}>
-                  <p>{release}</p>
-                  <p>{maturityRating}+</p>
-                  <p>{duration}</p>
-                </div>
-                <div className={styles.info__description}>
-                  <p>{description}</p>
-                </div>
-              </div>
-              <div className={styles.info__container}>
-                <p>
-                  Cast:
-                  {cast.map((actor, id) => {
-                    if (id < 3) {
-                      return <a href="#">{actor},</a>;
-                    }
-                    return;
-                  })}
-                  <a href="#">more...</a>
-                </p>
+            ></div>
 
-                <p>
+            <div className={styles.modal}>
+              <button
+                className={styles.modal__closeBtn}
+                onClick={handleResetSelectedMovie}
+              >
+                X
+              </button>
+
+              <div className={styles.modal__header}>
+                <div className={styles.modal__header__video}>
+                  <img
+                    className={styles.modal__header__video__videoImage}
+                    src={`/movies/${nameSlug}/${nameSlug}-video-cover.jpg`}
+                    alt=""
+                  />
+                  <img
+                    className={styles.modal__header__video__movieLogo}
+                    src={`/movies/${nameSlug}/${nameSlug}-logo.png`}
+                    alt=""
+                  />
+                </div>
+              </div>
+
+              {/*------------------ MODAL INFO ------------------*/}
+              <div className={styles.modal__info}>
+                <div className={styles.modal__info__container}>
+                  <div className={styles.modal__info__stats}>
+                    <p className={styles.modal__info__stats__release}>
+                      {release}
+                    </p>
+                    <p className={styles.modal__info__stats__maturityRating}>
+                      {maturityRating}+
+                    </p>
+                    <p className={styles.modal__info__stats__duration}>
+                      {duration}
+                    </p>
+                  </div>
+                  <div className={styles.modal__info__description}>
+                    <p className={styles.modal__info__description__description}>
+                      {description}
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.modal__info__container}>
+                  <p className={styles.modal__info__container__cast}>
+                    Cast:
+                    {cast.map((actor, id) => {
+                      if (id < 3) {
+                        return (
+                          <a key={`modal-cast-${actor}`} href="#">
+                            {actor},
+                          </a>
+                        );
+                      }
+                      return;
+                    })}
+                    <a href="#">more...</a>
+                  </p>
+                  <p className={styles.modal__info__container__genres}>
+                    Genres:
+                    {genres.map((genre) => (
+                      <a key={`modal-genre-${genre}`} href="#">
+                        {genre},
+                      </a>
+                    ))}
+                  </p>
+                  <p className={styles.modal__info__container__thisMovieIs}>
+                    This movie is:
+                    {thisMovieIs.map((item) => (
+                      <a key={`modal-thisMovieIs-${item}`} href="#">
+                        {item},
+                      </a>
+                    ))}
+                  </p>
+                </div>
+              </div>
+
+              {/*------------------ MORE LIKE THIS ------------------*/}
+              {moreLikeThisArray.length !== 0 && (
+                <div className={styles.modal__moreLikeThis}>
+                  <h2>More like this</h2>
+                  {moreLikeThisArray.map((item) => {
+                    return (
+                      <img
+                        key={`modal-moreLikeThis-${item.name}`}
+                        src={`/movies/${item.nameSlug}/${item.nameSlug}-mini.jpg`}
+                      />
+                    );
+                  })}
+                </div>
+              )}
+
+              {/*------------------ MODAL ABOUT ------------------*/}
+              <div className={styles.modal__about}>
+                <h2 className={styles.modal__about__movieName}>About {name}</h2>
+                <p className={styles.modal__about__director}>
+                  Director:
+                  {directors.map((director) => (
+                    <a key={`modal-about-director-${director}`} href="#">
+                      {director},
+                    </a>
+                  ))}
+                </p>
+                <p className={styles.modal__about__cast}>
+                  Cast:
+                  {cast.map((actor) => (
+                    <a key={`modal-about-cast-${actor}`} href="#">
+                      {actor},
+                    </a>
+                  ))}
+                </p>
+                <p className={styles.modal__about__writer}>
+                  Writer:
+                  {writer.map((author) => (
+                    <a key={`modal-about-writer-${author}`} href="#">
+                      {author},
+                    </a>
+                  ))}
+                </p>
+                <p className={styles.modal__about__genres}>
                   Genres:
                   {genres.map((genre) => (
-                    <a href="#">{genre},</a>
+                    <a key={`modal-about-genres-${genre}`} href="#">
+                      {genre},
+                    </a>
                   ))}
                 </p>
 
-                <p>
+                <p className={styles.modal__about__thisMovieIs}>
                   This movie is:
                   {thisMovieIs.map((item) => (
-                    <a href="#">{item},</a>
+                    <a key={`modal-about-thisMovieIs-${item}`} href="#">
+                      {item},
+                    </a>
                   ))}
                 </p>
+                <p className={styles.modal__about__maturityRating}>
+                  Maturity Rating: {maturityRating}+ Recommended for ages{" "}
+                  {maturityRating} and up
+                </p>
               </div>
-            </div>
-
-            <div className={styles.modal__about}>
-              <h2>About {name}</h2>
-              <p>
-                Cast:
-                {directors.map((director) => (
-                  <a href="#">{director},</a>
-                ))}
-              </p>
-              <p>
-                Cast:
-                {cast.map((actor) => (
-                  <a href="#">{actor},</a>
-                ))}
-              </p>
-              <p>
-                Writer:
-                {writer.map((author) => (
-                  <a href="#">{author},</a>
-                ))}
-              </p>
-              <p>
-                Genres:
-                {genres.map((genre) => (
-                  <a href="#">{genre},</a>
-                ))}
-              </p>
-
-              <p>
-                This movie is:
-                {thisMovieIs.map((item) => (
-                  <a href="#">{item},</a>
-                ))}
-              </p>
-              <p>
-                Maturity Rating: {maturityRating}+ Recommended for ages{" "}
-                {maturityRating} and up
-              </p>
             </div>
           </div>
         </>
