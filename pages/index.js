5f4ffbec128333a5dc0b4330adc39de3d28b1a3e
p-alex <alexey9021@gmail.com>
2021-08-10 14:16:29 +0300

    Added PageWrapperStyles css module to prevent scrolling when modal is active

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Banner from "../components/Banner";
 import FullscreenLoader from "../components/FullscreenLoader";
 import MovieSlider from "../components/MovieSlider";
 import Modal from "../components/Modal";
+import pageWrapperStyles from "../styles/PageWrapperStyles.module.css";
 export default function Home({ username, profileImg }) {
   const context = useContext(ProjectContext);
   const { selectedMovie, allMovies, isLoading, handleGetAllMovies } = context;
