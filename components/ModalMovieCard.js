14f77e0e1b2cb530fc322cd67b1e34dd843a9994
p-alex <alexey9021@gmail.com>
2021-08-14 12:47:55 +0300

    Added a play button and the duration of the movie to ModalMovieCard

--- components/ModalMovieCard.js
+++ components/ModalMovieCard.js
@@ -4,7 +4,8 @@ import styles from "../styles/ModalMovieCard.module.css";
 export default function ModalMovieCard({ movie, isMinThree }) {
   const context = useContext(ProjectContext);
   const { handleAddMovieToList, movieList } = context;
-  const { nameSlug, maturityRating, release, description, _id } = movie;
+  const { nameSlug, maturityRating, release, description, _id, duration } =
+    movie;
   return (
     <div
       className={
@@ -13,10 +14,19 @@ export default function ModalMovieCard({ movie, isMinThree }) {
           : styles.modalMovieCard + " " + styles.modalMovieCard_maxWidth
       }
     >
-      <img
-        className={styles.modalMovieCard__image}
-        src={`/movies/${nameSlug}/${nameSlug}-mini.jpg`}
-      />
+      <div className={styles.modalMovieCard__cardHeader}>
+        <p className={styles.modalMovieCard__cardHeader__duration}>
+          {duration}
+        </p>
+        <div className={styles.modalMovieCard__cardHeader__playBtn}>
+          <i className="fas fa-play"></i>
+        </div>
+        <img
+          className={styles.modalMovieCard__cardHeader__image}
+          src={`/movies/${nameSlug}/${nameSlug}-mini.jpg`}
+        />
+      </div>
+
       <div className={styles.modalMovieCard__cardBody}>
         <div className={styles.modalMovieCard__cardBody__infoAndAdd}>
           <div className={styles.modalMovieCard__cardBody__infoAndAdd__info}>
