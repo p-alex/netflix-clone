4a9d81cb92f4160826b48f9082be899534c01453
p-alex <alexey9021@gmail.com>
2021-08-15 20:47:51 +0300

    Update add-movie-to-list.js

--- pages/api/add-movie-to-list.js
+++ pages/api/add-movie-to-list.js
@@ -5,29 +5,41 @@ async function addMovieToListHandler(req, res) {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
   if (req.method === "POST") {
     try {
       const usersCollection = client.db().collection("users");
+
       const user = await usersCollection.findOne({ _id: ObjectId(req.userId) });
+
       const movieId = req.body.movieId;
+
       if (movieId) {
         let oldMovieList = user.movieList;
+
         if (!oldMovieList.includes(movieId)) {
+          // ADDING MOVIE ID TO MOVIE LIST
           let updatedMovieList = [...oldMovieList, movieId];
+
           console.log(updatedMovieList);
+
           const updateMovieList = await usersCollection.updateOne(
             { _id: ObjectId(req.userId) },
             { $set: { movieList: updatedMovieList } }
           );
+
           if (updateMovieList.result.ok) {
             res.json({ message: "Movie added to list" });
           }
         } else {
+          // REMOVING MOVIE ID FROM MOVIE LIST
           let updatedMovieList = oldMovieList.filter((id) => id !== movieId);
+
           const updateMovieList = await usersCollection.updateOne(
             { _id: ObjectId(req.userId) },
             { $set: { movieList: updatedMovieList } }
           );
+
           if (updateMovieList.result.ok) {
             res.json({ message: "Movie removed from list" });
           }
