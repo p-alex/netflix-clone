12f7037378e8f25904e02d2d386c0dbe319c770b
p-alex <alexey9021@gmail.com>
2021-08-23 16:46:26 +0300

    Added Button component

---
+++ components/Button.js
@@ -0,0 +1,26 @@
+import styles from "../styles/Button.module.css";
+export default function Button({ type, func, value }) {
+  return (
+    <>
+      <button
+        className={
+          styles.banner__content__btn_container__btn +
+          " " +
+          styles.banner__content__btn_container__play
+        }
+        onClick={func}
+      >
+        <i
+          className={
+            type === "play"
+              ? "fas fa-play"
+              : type === "moreInfo"
+              ? "fas fa-info-circle"
+              : null
+          }
+        ></i>
+        <span>{value}</span>
+      </button>
+    </>
+  );
+}
