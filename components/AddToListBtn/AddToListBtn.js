8137f5644740f1c90835fe76eae4d996e653f4ee
p-alex <alexey9021@gmail.com>
2021-09-14 19:54:06 +0300

    Added infoPopup to AddToListBtn

--- components/AddToListBtn/AddToListBtn.js
+++ components/AddToListBtn/AddToListBtn.js
@@ -8,31 +8,43 @@ export default function AddToListBtn({ movieId, btnType }) {
   return (
     <>
       {userMovieList && userMovieList.some((item) => item === movieId) ? (
-        <button
-          className={
-            btnType === "regular"
-              ? styles.addToListRegularBtn + " " + styles.movieIsAdded
-              : btnType === "rounded"
-              ? styles.addToListRoundedBtn + " " + styles.movieIsAdded
-              : styles.addToListRegularBtn + " " + styles.movieIsAdded
-          }
-          onClick={() => handleAddMovieToList(movieId, false)}
-        >
-          <i className="fas fa-check"></i>
-        </button>
+        <div className={styles.btnContainer}>
+          <div className={styles.infoPopup}>
+            <p>Remove from My List</p>
+            <div className={styles.infoPopup__triangle}></div>
+          </div>
+          <button
+            className={
+              btnType === "regular"
+                ? styles.addToListRegularBtn + " " + styles.movieIsAdded
+                : btnType === "rounded"
+                ? styles.addToListRoundedBtn + " " + styles.movieIsAdded
+                : styles.addToListRegularBtn + " " + styles.movieIsAdded
+            }
+            onClick={() => handleAddMovieToList(movieId, false)}
+          >
+            <i className="fas fa-check"></i>
+          </button>
+        </div>
       ) : (
-        <button
-          className={
-            btnType === "regular"
-              ? styles.addToListRegularBtn
-              : btnType === "rounded"
-              ? styles.addToListRoundedBtn
-              : styles.addToListRegularBtn
-          }
-          onClick={() => handleAddMovieToList(movieId, true)}
-        >
-          <i className="fas fa-plus"></i>
-        </button>
+        <div className={styles.btnContainer}>
+          <div className={styles.infoPopup}>
+            <p>Add to My List</p>
+            <div className={styles.infoPopup__triangle}></div>
+          </div>
+          <button
+            className={
+              btnType === "regular"
+                ? styles.addToListRegularBtn
+                : btnType === "rounded"
+                ? styles.addToListRoundedBtn
+                : styles.addToListRegularBtn
+            }
+            onClick={() => handleAddMovieToList(movieId, true)}
+          >
+            <i className="fas fa-plus"></i>
+          </button>
+        </div>
       )}
     </>
   );
