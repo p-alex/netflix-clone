4bdd860bb8c7123da08796b4b25e1ba3cfccba57
p-alex <alexey9021@gmail.com>
2021-10-02 17:52:00 +0300

    Enabled account creation

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -3,7 +3,7 @@ import cookie from "cookie";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import sanitize from "mongo-sanitize";
-//import sgMail from "@sendgrid/mail";
+import sgMail from "@sendgrid/mail";
 export default async function authHandler(req, res) {
   const client = await MongoClient.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -128,40 +128,38 @@ export default async function authHandler(req, res) {
           }
         );
 
-        // sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 
-        // const msg = {
-        //   to: email,
-        //   from: "netflixclonepalex@gmail.com",
-        //   subject: "Verification link",
-        //   text: "Click the button below to verify your account",
-        //   html: `<div style='text-align:center;position:relative; width:400px;padding:40px;margin:0 auto;background-color:black;color:white;'><h1>Click the link to verify your account.</h1><br/><br/><a style='display:inline-block;text-decoration:none;background-color:#e50914;padding:15px;color:white;border-radius:5px;font-weight:bold;font-family:Helvetica, sans-serif;' href="https://netflix-clone-inky-five.vercel.app/user/verify/${token}">Confirm account</a></div>`,
-        // };
+        const msg = {
+          to: email,
+          from: "netflixclonepalex@gmail.com",
+          subject: "Email verification link from Netflixpalexclone",
+          text: "Click the button below to verify your account",
+          html: `<div style='text-align:center;position:relative; width:400px;padding:40px;margin:0 auto;background-color:black;color:white;font-family:Helvetica, sans-serif'><h1>Hi ${username},</h1><br/><br/><p style="color:white;font-size:1.1rem">You have successfully created a Netflixpalexclone account.<br/>Please click on the link below to verify your email address and complete your registration.</p><br/><br/><a style='display:inline-block;text-decoration:none;background-color:#e50914;padding:15px;color:white;border-radius:5px;font-weight:bold;font-size:1.4rem;font-family:Helvetica, sans-serif;' href="https://netflix-clone-inky-five.vercel.app/user/verify/${token}" rel="noreferrer">Verify your email</a></div>`,
+        };
 
-        // sgMail
-        //   .send(msg)
-        //   .then(() => {
-        //     console.log("Email sent");
-        //     return res.json({
-        //       ok: 1,
-        //       message:
-        //         "Success! We sent you an email to verify your account! Please check your email.",
-        //     });
-        //   })
-        //   .catch((error) => {
-        //     console.error(error);
-        //     return res.json({
-        //       ok: 0,
-        //       message: "Something went wrong! Please try again later.",
-        //     });
-        //   });
+        sgMail
+          .send(msg)
+          .then(() => {
+            return res.json({
+              ok: 1,
+              message:
+                "Success! We sent you an email to verify your account! Please check your email.",
+            });
+          })
+          .catch((error) => {
+            return res.json({
+              ok: 0,
+              message: "Something went wrong! Please try again later.",
+            });
+          });
 
-        console.log(`http://localhost:3000/user/verify/${token}`);
-        return res.json({
-          ok: 1,
-          message:
-            "Success! We sent you an email to verify your account! Please check your email.",
-        });
+        // console.log(`http://localhost:3000/user/verify/${token}`);
+        // return res.json({
+        //   ok: 1,
+        //   message:
+        //     "Success! We sent you an email to verify your account! Please check your email.",
+        // });
       }
       if (authType === "login") {
         //-----------LOGIN-----------
