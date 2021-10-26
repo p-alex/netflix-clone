e297b757f14225a583b92c6effa379e3fec5792e
p-alex <alexey9021@gmail.com>
2021-10-26 21:34:04 +0300

    Added Footer component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,88 @@
+import styles from "./Footer.module.css";
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.footer__social}>
+        <li>
+          <a
+            href="https://github.com/p-alex"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="My Github"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/alexandru-daniel-pistol"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="My LinkedIn"
+          >
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
+      <ul className={styles.footer__links}>
+        <li>
+          <a href="#">Audio and Subtitles</a>
+        </li>
+        <li>
+          <a href="#">Audio Descripton</a>
+        </li>
+        <li>
+          <a href="#">Help Center</a>
+        </li>
+        <li>
+          <a href="#">Gift Cards</a>
+        </li>
+        <li>
+          <a href="#">Media Center</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+        <li>
+          <a href="#">Jobs</a>
+        </li>
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+        <li>
+          <a href="#">Privacy</a>
+        </li>
+        <li>
+          <a href="#">Legal Notices</a>
+        </li>
+        <li>
+          <a href="#">Cookie Preferences</a>
+        </li>
+        <li>
+          <a href="#">Corporate Information</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+      <div className={styles.footer__bottom}>
+        <small>
+          Netflix Clone created by{" "}
+          <a
+            href="https://www.linkedin.com/in/alexandru-daniel-pistol"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="My LinkedIn"
+          >
+            Alex Daniel
+          </a>
+          .
+        </small>
+        <small>
+          This application was created just to be added to my portfolio so i can
+          maybe land job.
+        </small>
+      </div>
+    </footer>
+  );
+}
