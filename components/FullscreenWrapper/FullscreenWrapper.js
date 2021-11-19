65be93aac69823b2867841672d73ae058ef46198
p-alex <alexey9021@gmail.com>
2021-11-19 18:36:24 +0200

    Added background to FullscreenWrapper

--- components/FullscreenWrapper/FullscreenWrapper.js
+++ components/FullscreenWrapper/FullscreenWrapper.js
@@ -1,7 +1,10 @@
 import styles from "./FullscreenWrapper.module.css";
 export default function FullscreenWrapper({ children }) {
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={styles.wrapper}
+      style={{ backgroundImage: "url(/images/bg/auth-bg.webp)" }}
+    >
       <div className={styles.black_overlay}></div>
       {children}
     </div>
